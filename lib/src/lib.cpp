2e3f0d1ff7119cdbe44d8ded8885c0a0a777810d
Greg Walker <greg_walker@sympatico.ca>
2022-04-16 10:21:49 -0400

    Copied in provided code

--- lib/src/lib.cpp
+++ lib/src/lib.cpp
@@ -15,6 +15,30 @@ using namespace std;
 //    return "Hello " + personName;
 //}
 
-int pm_main() {
+/*
+ * Complete the 'minSwapsRequired' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int minSwapsRequired(string s) {
+
+    return 0;
+}
+
+int pm_main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = minSwapsRequired(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
     return 0;
 }
