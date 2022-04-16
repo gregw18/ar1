37a69bf1014e9095109e1dec959fa64ed1280bfd
Greg Walker <greg_walker@sympatico.ca>
2022-04-16 10:51:48 -0400

    Added comment with approach

--- lib/src/lib.cpp
+++ lib/src/lib.cpp
@@ -22,6 +22,33 @@ using namespace std;
  * The function accepts STRING s as parameter.
  */
 
+/*
+    Note to reviewer: I don't like the HackerRank environment for programming - I can't write and run
+                        my own tests. Thus, I set up a local environment, with a template project
+                        of my own design, in VS Code and copied the resulting code into hackerrank
+                        when complete. The complete evolution of the program can be found at 
+                        github.com\gregw18\ar1.
+    Problem:
+    Given a binary string, swap any two characters, as many times as necessary, to create a palindrome.
+    Return the minimum number of swaps required. Return -1 if impossible.
+    Notes:
+        1. If have even number of digits, but odd number of 1's (and thus, also 0's), won'd be able
+            to create a palindrome. Otherwise, should be possible.
+        2. If have odd number of digits, need one of the odd digits in the middle.
+        3. 
+    Approach:
+        Check if possible. Return -1 if not.
+        Create 2 vectors. First is list of indexes of 0's that are out of place - i.e. the mirrorred
+        item in the string doesn't match. Don't include middle item, if odd number of items.
+        Second is list of indexes of 1's that are out of place. Again, don't include middle item.
+        If swap first item from 0's vector with 2nd item from 1's vector, fixed both. If length of
+        one list is even, n = length/2, else roundup(length/2).
+        If both lists contain just one item, swap those, n = 1.
+        isPalindromePossible(string)
+        createMismatchedLists(string)
+        calcSwapsRequired(vector, vector)
+*/
+
 int minSwapsRequired(string s) {
 
     return 0;
